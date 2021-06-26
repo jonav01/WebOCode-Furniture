25fc9c7781df819b155d133df621ec85693affbc
Shreyon Sinha <jonescomputer443@gmail.com>
2021-06-26 20:46:15 +0530

    Added the functionality of changing images in the header section Background , Added a new Section called Categories

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ window.addEventListener("scroll", (e) => {
     navbar.style.backgroundColor = "transparent";
   }
 });
+
+const hamburger = document.querySelector(".hamburger__icon");
+const nav = document.querySelector(".navbar");
+hamburger.addEventListener("click", (e) => {
+  nav.classList.toggle("open");
+});
+
+// header background change
+const header = document.querySelector(".header");
+let i = 1;
+setInterval(() => {
+  if (i === 4) i = 0;
+  header.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url("./assets/header__images/header__background${
+    i + 1
+  }.jfif")`;
+  i++;
+}, 10000);
